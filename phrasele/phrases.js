7e07faaa46a4839dcce2a7918caab8778dfeed88
Anesti101 <anestigeb@gmail.com>
2025-12-28 00:13:40 +0000

    Update phrases and hints for improved gameplay experience

--- phrasele/phrases.js
+++ phrasele/phrases.js
@@ -1,9 +1,9 @@
 const PHRASES = [
-  { text: "HOW WE MET", hint: "Where it all started" },
-  { text: "OUR FIRST DATE", hint: "That night" },
-  { text: "I LOVE YOU", hint: "Three words" },
-  { text: "MY FOREVER PERSON", hint: "Who you are to me" },
-  { text: "WILL YOU MARRY ME", hint: "One final question 💍" }
+  { text: "FINAL", hint: "Where it all started" },
+  { text: "NIGHT", hint: "That night" },
+ { text: "Three", hint: "Three words" },
+  { text: "Who", hint: "Who you are to me" },
+  { text: "all", hint: "One final question 💍" }
 ];
 
 
