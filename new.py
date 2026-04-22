aef6f564eccdec8bf2385b649fdba19ca35eb04f
Anesti101 <anestigeb@gmail.com>
2026-04-22 16:56:28 +0100

    added some files

--- new.py
+++ new.py
@@ -4,12 +4,7 @@ import sys
 filename = sys.argv[1]
 
 with open(filename) as f:
-    line = f.read().strip()
+    lines = f.readlines().strip()
 
-a, b = line.split(",")
-
-
-def add(a,b):
-    print(a+b) 
-
-add(int(a),int(b))
+    for i in lines:
+        return sum(i)
